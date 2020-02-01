1c7ab7285aced9d4261b962ad444b77071b02c18
DESKTOP-GOMR1GS\Anon <matipre96@gmail.com>
2020-02-01 09:39:59 -0300

    Object pieces vuelan a la bosta

---
+++ Assets/Scripts/BrokenObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenObject : MonoBehaviour
+{
+    public Rigidbody[] rigidbodies;
+    public float force = 20;
+    bool activated = false;
+
+    public void OnPieceTouchFloor(Vector3 collidePoint)
+    {
+        if (activated)
+            return;
+        activated = true;
+        foreach (var piece in rigidbodies)
+        {
+            var dir = piece.transform.position - collidePoint;
+            Debug.Log(dir);
+            piece.AddForce(dir * force, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/PieceColliderTriggerer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PieceColliderTriggerer : MonoBehaviour
+{
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 9)
+            return;
+        transform.GetComponentInParent<BrokenObject>().OnPieceTouchFloor(collision.contacts.First().point);
+  
+    }
+}
