8a5b0f65a3467ad950ff550cbe8f6724955017d2
Fernando Hermes <fernando.hermes@etermax.com>
2020-02-01 11:50:25 -0300

    PieceCollection

---
+++ Assets/PieceCollection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceCollection : MonoBehaviour
+{
+    [SerializeField] private int rayLength = 2;
+    [SerializeField] private LayerMask layerMaskInteract;
+    private float distance;
+    private Rigidbody pieceBody;
+
+    void Update()
+    {
+        RaycastHit hit;
+        var fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMaskInteract.value))
+        {
+            var piece = hit.collider.gameObject;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                pieceBody = piece.GetComponent<Rigidbody>();
+                distance = (hit.point - piece.transform.position).magnitude;
+            }
+
+            if (Input.GetButton("Fire1"))
+            {
+                pieceBody.velocity = Vector3.zero;
+                pieceBody.angularVelocity = Vector3.zero;
+                pieceBody.MovePosition(transform.position + transform.forward * (distance + 1.5f));
+                pieceBody.MoveRotation(transform.rotation);
+            }
+        }
+    }
+}
