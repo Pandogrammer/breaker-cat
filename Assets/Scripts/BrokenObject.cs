c549ef5c197877127d58c5daf685fc97176d1cf2
Matias Nievas Soto <matias.nievas@etermax.com>
2020-02-01 13:18:40 -0300

    se implementan estados de los objetos

--- Assets/Scripts/BrokenObject.cs
+++ Assets/Scripts/BrokenObject.cs
@@ -1,58 +1,72 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using System.Linq;
+using DefaultNamespace;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class BrokenObject : MonoBehaviour
 {
     public Rigidbody[] rigidbodies;
     public Transform healthyObject;
     public float force = 20;
-    bool activated;
-    bool onFixed;
     public float minDistantce = 10f;
-    private float timer = 0f;
-    private float smokeTimer = 0f;
     public float rangeTimer = 3f;
     public float smokeRangeTimer = 5f;
     public GameObject smoke;
-    
+
+    private float timeToBreak = 0f;
+    private float smokeTimer = 0f;
+    bool activated;
+    bool onFixed;
+    private States state = States.Healthy;
+
+    private void Awake()
+    {
+        state = States.Drop;
+    }
+
     private void Update()
     {
-        
-        if (activated)
-        {
-            timer += Time.deltaTime;
-            if (timer > rangeTimer)
-            {
-                StartFixAnimation();
-            }
-        }
-
-        if (onFixed)
-        {
-            FixAnimation();
-        }
+        Debug.Log("state: " + state);
+        OnCrash();
+        StartFixAnimation();
+        FixAnimation();
     }
 
     public void OnPieceTouchFloor(Vector3 collidePoint)
     {
-        if (activated)
+        if (state != States.Drop)
             return;
-        activated = true;
+        
         foreach (var piece in rigidbodies)
         {
             var dir = piece.transform.position - collidePoint;
             Debug.Log(dir);
             piece.AddForce(dir * force, ForceMode.Impulse);
         }
+        state = States.Crash;
+    }
+
+    private void OnCrash()
+    {
+        if (state != States.Crash)
+            return;
+        
+        timeToBreak += Time.deltaTime;
+        if (timeToBreak > rangeTimer)
+            state = States.Broken;
     }
 
     private void StartFixAnimation()
     {
+        if(state != States.Broken)
+            return;
+        
         var sum = rigidbodies.Aggregate(Vector3.zero, (current, piece) => current + piece.transform.position);
         if (rigidbodies.All(piece => Vector3.Distance(sum, piece.transform.position) < minDistantce))
         {
             gameObject.transform.position = sum;
-            onFixed = true;
+            state = States.Fixed;
             smoke.SetActive(true);
             foreach (var piece in rigidbodies)
             {
@@ -63,12 +77,16 @@ public class BrokenObject : MonoBehaviour
 
     private void FixAnimation()
     {
+        if(state != States.Fixed)
+            return;
+        
         smokeTimer += Time.deltaTime;
         if (smokeTimer > smokeRangeTimer)
         {
             gameObject.transform.rotation = Quaternion.identity;
             healthyObject.gameObject.SetActive(true);
             smoke.SetActive(false);
+            state = States.Healthy;
         }
     }
 }
---
+++ Assets/Scripts/States.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace
+{
+    public enum States
+    {
+        Drop, Crash, Broken, Fixed, Healthy
+    }
+}
