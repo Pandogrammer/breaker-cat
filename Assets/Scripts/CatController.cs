b1588695909a95f83cdb1aa69a808e7ca89bbd05
DESKTOP-GOMR1GS\Anon <matipre96@gmail.com>
2020-02-01 11:57:52 -0300

    Basic cat teleportation

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public event Action OnActionFinished;
+
+    public void TeleportTo(Vector3 position, Vector3 forward)
+    {
+        transform.position = position;
+        transform.forward = forward;
+        StartCoroutine(TeleportTimer());
+    }
+
+    IEnumerator TeleportTimer()
+    {
+        yield return new WaitForSeconds(3f);
+        OnActionFinished?.Invoke();
+    }
+}
---
+++ Assets/Scripts/CatTeleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatTeleporter : MonoBehaviour
+{
+    public CatController cat;
+    public CatTeleportPosition[] catTeleportPositions;
+    private int currentTeleport = 0;
+
+    private void Start()
+    {
+        NextTeleport();
+        cat.OnActionFinished += NextTeleport;
+    }
+
+    void NextTeleport()
+    {
+        if (currentTeleport > catTeleportPositions.Length-1)
+            return;
+        cat.TeleportTo(
+            catTeleportPositions[currentTeleport].transform.position,
+            catTeleportPositions[currentTeleport].transform.forward);
+        currentTeleport++;
+    }
+}
