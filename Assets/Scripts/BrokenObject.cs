868c9057f6ec2eec105116c20782cc98c4fa14da
Matias Nievas Soto <matias.nievas@etermax.com>
2020-02-01 10:51:13 -0300

    primer acercamiento a rearmar un objeto

--- Assets/Scripts/BrokenObject.cs
+++ Assets/Scripts/BrokenObject.cs
@@ -1,12 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
 
 public class BrokenObject : MonoBehaviour
 {
     public Rigidbody[] rigidbodies;
+    public Transform healthyObject;
     public float force = 20;
     bool activated = false;
+    public float minDistantce = 10f;
+    private float timer = 0f;
+    public float rangeTimer = 10f;
+    
+    private void Update()
+    {
+        
+        if (activated)
+        {
+            timer += Time.deltaTime;
+            if (timer > rangeTimer)
+            {
+                var sum = rigidbodies.Aggregate(Vector3.zero, (current, piece) => current + piece.transform.position);
+                if (rigidbodies.All(piece => Vector3.Distance(sum, piece.transform.position)< minDistantce))
+                {
+                    foreach (var piece in rigidbodies)
+                    {
+                        piece.gameObject.SetActive(false);
+                    }
+                    healthyObject.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
 
     public void OnPieceTouchFloor(Vector3 collidePoint)
     {
