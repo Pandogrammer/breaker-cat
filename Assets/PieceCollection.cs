77cdd1091e1b82162f22e578ae2f7188113154df
Fernando Hermes <fernando.hermes@etermax.com>
2020-02-01 12:26:51 -0300

    PieceCollection

--- Assets/PieceCollection.cs
+++ Assets/PieceCollection.cs
@@ -4,32 +4,37 @@ using UnityEngine;
 
 public class PieceCollection : MonoBehaviour
 {
-    [SerializeField] private int rayLength = 2;
+    [SerializeField] private float rayLength = 0.1f;
     [SerializeField] private LayerMask layerMaskInteract;
+    [SerializeField] private float radius = 1f;
     private float distance;
     private Rigidbody pieceBody;
+    private bool holding;
 
     void Update()
     {
         RaycastHit hit;
         var fwd = transform.TransformDirection(Vector3.forward);
 
-        if (Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMaskInteract.value))
+        if (Physics.SphereCast(transform.position, radius, fwd, out hit, rayLength, layerMaskInteract.value))
         {
             var piece = hit.collider.gameObject;
             if (Input.GetButtonDown("Fire1"))
             {
+                holding = !holding;
                 pieceBody = piece.GetComponent<Rigidbody>();
                 distance = (hit.point - piece.transform.position).magnitude;
             }
-
-            if (Input.GetButton("Fire1"))
-            {
-                pieceBody.velocity = Vector3.zero;
-                pieceBody.angularVelocity = Vector3.zero;
-                pieceBody.MovePosition(transform.position + transform.forward * (distance + 1.5f));
-                pieceBody.MoveRotation(transform.rotation);
-            }
         }
+        if(holding)
+            MoveObject();
+    }
+
+    private void MoveObject()
+    {
+        pieceBody.velocity = Vector3.zero;
+        pieceBody.angularVelocity = Vector3.zero;
+        pieceBody.MovePosition(transform.position + transform.forward * (distance + 1.5f));
+        pieceBody.MoveRotation(transform.rotation);
     }
 }
