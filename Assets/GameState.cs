2fddbfd7bacb6885bdb9b6881c63db2cd8a71e03
Fernando Hermes <fernando.hermes@etermax.com>
2020-02-01 16:11:42 -0300

    Agregado timer

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -1,20 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    private int score;
-    private List<BreakableObject> objects;
     private UIController uiController;
+    private List<BreakableObject> objects;
+
+    [SerializeField] private float doomsday = 10;
+    private int score;
 
     void Awake()
     {
         uiController = FindObjectOfType<UIController>();
         objects = FindObjectsOfType<BreakableObject>().ToList();
         objects.ForEach(SubscribeToScore);
-        
-        uiController.Setup(objects.Count);
+        uiController.Setup(objects.Count, (int)doomsday);
+    }
+
+    void Update()
+    {
+        CheckDoomsday();
+
+    }
+
+    private void CheckDoomsday()
+    {
+        if(doomsday > 0){
+            doomsday -= Time.deltaTime;
+            uiController.SetTimer(Mathf.CeilToInt(doomsday));
+            return;
+        }
+
+        if (score == objects.Count)
+        {
+            uiController.ShowWin();
+        }
+        else
+        {
+            uiController.ShowLose();
+        }
     }
 
     private void SubscribeToScore(BreakableObject obj)
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,16 +7,38 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI finishText;
+    [SerializeField] private TextMeshProUGUI timeText;
     private int objectCount;
 
     public void SetScore(int score)
     {
-        scoreText.text = string.Format("OBJECTS REPAIRED: {0}/{1}", score, objectCount);
+        scoreText.text = $"REPAIRED: {score}/{objectCount}";
     }
 
-    public void Setup(int objectCount)
+    public void Setup(int objectCount, int time)
     {
         this.objectCount = objectCount;
+        SetTimer(time);
         SetScore(0);
+    }
+
+    public void ShowWin()
+    {
+        timeText.gameObject.SetActive(false);
+        finishText.text = "YOU WIN!";
+        finishText.gameObject.SetActive(true);
+    }
+
+    public void ShowLose()
+    {
+        timeText.gameObject.SetActive(false);
+        finishText.text = "YOU LOSE :c";
+        finishText.gameObject.SetActive(true);
+    }
+
+    public void SetTimer(int time)
+    {
+        timeText.text = $"TIME: {time.ToString()}";
     }
 }
