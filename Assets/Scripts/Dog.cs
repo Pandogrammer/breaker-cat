c0a1564d660a1ad84ee509bd8aa590f1080ab48d
Fernando Hermes <fernando.hermes@etermax.com>
2020-02-02 09:19:34 -0300

    Cambiados de lugar los scripts

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private MouseLook mouseLook;
+    [SerializeField] private Movement movement;
+    public event Action OnGameStarted;
+    public void StartGame()
+    {
+        animator.SetTrigger("CameraCloseUp");
+    }
+
+    public void Update()
+    {
+        if (animator.GetBool("Started"))
+        {
+            animator.SetBool("Started", false);
+            OnGameStarted();
+        }
+    }
+
+    public void DisableControls()
+    {
+        mouseLook.enabled = false;
+        movement.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
