0a5141684eb2e756884a6c43b47965f7fd1b6b1a
Matias Nievas Soto <matias.nievas@etermax.com>
2020-02-02 08:40:35 -0300

    Se agrega música de intro

--- Assets/MenuScreenController.cs
+++ Assets/MenuScreenController.cs
@@ -1,22 +1,44 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuScreenController : MonoBehaviour
 {
     private readonly string scene = "MovementScene";
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip menuMusic;
     [SerializeField] private Button playButton;
 
     private void Awake()
     {
         playButton.onClick.AddListener(StartGame);
+        CreateAudioSource();
+    }
+
+    private void CreateAudioSource()
+    {
+        var newAudioSource = new GameObject("AudioSource").AddComponent<AudioSource>();
+        newAudioSource.loop = true;
+        audioSource = newAudioSource;
+    }
+
+    private void Start()
+    {
+        Play();
     }
 
     private void StartGame()
     {
         SceneManager.LoadScene(scene);
+    }
+    
+    void Play()
+    {
+        if (audioSource.isPlaying || !enabled)
+            return;
+
+        audioSource.clip = menuMusic;
+        audioSource.Play();
+
     }
 }
