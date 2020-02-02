39a24f4fc2f8d948e2c2db68b03d6d9c51a962c3
Fernando Hermes <fernando.hermes@etermax.com>
2020-02-02 09:17:48 -0300

    Agregada la cinematica de inicio y la transicion al juego

---
+++ Assets/Scripts/CinematicExit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CinematicExit : StateMachineBehaviour
+{
+    private readonly string scene = "MovementScene";
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Input.GetButtonDown("Fire1"))
+            SceneManager.LoadScene(scene);
+    }
+
+   
+}
