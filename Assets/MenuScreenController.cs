60ad0905a7616b44f6344d33d80aabe87de32e3d
Fernando Hermes <fernando.hermes@etermax.com>
2020-02-01 21:13:44 -0300

    Agregada pantalla de inicio

---
+++ Assets/MenuScreenController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScreenController : MonoBehaviour
+{
+    private readonly string scene = "MovementScene";
+    [SerializeField] private Button playButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
