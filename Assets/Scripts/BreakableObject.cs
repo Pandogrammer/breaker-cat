432e580b39277f1a358aab6034561af530c0ff96
DESKTOP-GOMR1GS\Anon <matipre96@gmail.com>
2020-02-02 15:28:36 -0300

    asd

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -35,7 +35,7 @@ public class BreakableObject : MonoBehaviour
         {
             ObjectRepaired?.Invoke();
             brokenObject.state = States.BackToStartingPosition;
-            brokenObject.healthyObject.position = placeholder.position + Vector3.up*1.75f;
+            brokenObject.healthyObject.position = placeholder.position + Vector3.up*1.1f;
             brokenObject.healthyObject.rotation = placeholder.rotation;
             brokenObject.healthyObject.GetComponent<Rigidbody>().isKinematic = true;
             brokenObject.healthyObject.tag = "OnStartingPosition";
