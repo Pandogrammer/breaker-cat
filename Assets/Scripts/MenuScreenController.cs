e698fdd6d440fb75a42063134ceaa95c28c31d36
Fernando Hermes <fernando.hermes@etermax.com>
2020-02-02 13:52:10 -0300

    Agregados creditos al inicio

--- Assets/Scripts/MenuScreenController.cs
+++ Assets/Scripts/MenuScreenController.cs
@@ -8,11 +8,27 @@ public class MenuScreenController : MonoBehaviour
     private readonly string scene = "CinematicScene";
     [SerializeField] private Button playButton;
     [SerializeField] private IntroMusicGame introMusicGame;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Canvas creditsScreen;
+    [SerializeField] private Button canvasClick;
+
 
     private void Awake()
     {
         introMusicGame.PlayMusic += PlayMusic;
         playButton.onClick.AddListener(StartGame);
+        creditsButton.onClick.AddListener(ShowCredits);
+        canvasClick.onClick.AddListener(HideCredits);
+    }
+
+    private void HideCredits()
+    {
+        creditsScreen.gameObject.SetActive(false);
+    }
+
+    private void ShowCredits()
+    {
+        creditsScreen.gameObject.SetActive(true);
     }
 
     private void PlayMusic()
